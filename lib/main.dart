548d20ff1436d5b014236711673b29214f7bfa40
Your Name <you@example.com>
2024-12-06 11:38:30 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Страница Входа',
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('Войти'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Введите username',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Пожалуйста, введите username';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 30),
+              TextFormField(
+                obscureText: true, // Для показа звездочек при вводе пароля
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Введите пароль',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Пожалуйста, введите пароль';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState?.validate() ?? false) {
+                    // Если форма валидна, переходим на новый экран
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomePage()),
+                    );
+                  }
+                },
+                child: const Text('Войти'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Главная Страница'),
+      ),
+      body: const Center(
+        child: Text(
+          'Вы вошли в аккаунт',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
